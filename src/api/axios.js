15338fd35f1e6d85b86a09962e90e3c27ac1502a
wyw-s <wyw.s@foxmail.com>
2020-07-13 00:50:52 +0800

    更改请求的地址，及注释掉代理

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:3000'
+axios.defaults.baseURL = 'http://47.114.139.71:3000'
 axios.defaults.headers.post['Content-Type'] = 'application/json, charset=utf-8'
 
 const request = function (params) {
--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,17 @@ module.exports = {
   // 解决部署后访问空白页的问题；
   publicPath: process.env.NODE_ENV === 'production'
     ? '/dist/'
-    : '/',
-  devServer: {
-    proxy: 'http://localhost:3000'
-  }
+    : '/'
+  // devServer: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'http:47.114.139.71:3000',
+  //       changeOrigin: true,
+  //       ws: true,
+  //       pathRewrite: {
+  //         '^api': ''
+  //       }
+  //     }
+  //   }
+  // }
 }
