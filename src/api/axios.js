760813f5680a1c9954116803d949f111b4529bde
wyw-s <wyw.s@foxmail.com>
2020-07-15 18:02:37 +0800

    根据环境设置基地址，并配置代理

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://47.114.139.71:3000'
-axios.defaults.headers.post['Content-Type'] = 'application/json, charset=utf-8'
+const Axios = axios.create({
+  // 解决线上的请求404问题；
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://47.114.139.71:3000' : '',
+  // 此处不指定基地址因为配置的有 proxy 代理；
+  headers: {
+    'Content-Type': 'application/json, charset=utf-8'
+  }
+})
 
 const request = function (params) {
-  return axios(params)
+  return Axios(params)
 }
 
 export default request
--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,17 @@
 module.exports = {
   // 解决部署后访问空白页的问题；
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/dist/'
-    : '/'
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http:47.114.139.71:3000',
-  //       changeOrigin: true,
-  //       ws: true,
-  //       pathRewrite: {
-  //         '^api': ''
-  //       }
-  //     }
-  //   }
-  // }
+  publicPath: '/',
+  devServer: {
+    host: '0.0.0.0', // 允许以 ip 地址访问本机
+    port: 8085,
+    open: true,
+    proxy: {
+      '/hero': {
+        target: 'http://localhost:3000',
+        pathRewrite: {
+          '^/hero': '/hero'
+        }
+      }
+    }
+  }
 }
