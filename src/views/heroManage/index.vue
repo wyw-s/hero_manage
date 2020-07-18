91a65d22116546cba9959b96802f9f076209851e
wyw-s <wyw.s@foxmail.com>
2020-07-19 01:20:17 +0800

    图片上传压缩

--- src/views/heroManage/index.vue
+++ src/views/heroManage/index.vue
@@ -66,6 +66,7 @@
               ref="uploadImg"
               style="display: none"
               name="images"
+              accept="image/*"
               @change="onUploadImg"
             />
           </div>
@@ -124,7 +125,9 @@ export default {
       formLabelWidth: '120px',
       dialogFormVisible: false,
       isShowUploadBtn: true,
-      temporaryURL: ''
+      temporaryURL: '',
+      newUrlBase64: '',
+      fileName: ''
     }
   },
   created () {
@@ -154,15 +157,98 @@ export default {
       this.$refs.uploadImg.click()
     },
     onUploadImg (event) {
+      const this_ = this
       const file = event.target.files[0]
-      const url = URL.createObjectURL(file)
-      this.temporaryURL = url
-      this.isShowUploadBtn = false
+      if (file.type.includes('image')) {
+        // 1、创建img和fileReader的实例
+        const img = new Image()
+        const reader = new FileReader()// 读取文件资源
+        reader.readAsDataURL(file)
+        // 文件base64化
+        reader.onload = function (e) {
+          img.src = e.target.result
+        }
+        // base64地址图片加载完毕后
+        img.onload = function () {
+          // 获取图片实际宽高
+          const { width: originWidth, height: originHeight } = img
+          // 最大尺寸限制，设置宽高来实现图片压缩程度
+          const maxWidth = 100
+          const maxHeight = 60
+          // 目标尺寸
+          let targetWidth = originWidth
+          let targetHeight = originHeight
+          // 图片尺寸超过100x60的限制
+          if (originWidth > maxWidth || originHeight > maxHeight) {
+            if (originWidth / originHeight > maxWidth / maxHeight) {
+              // 按照宽度限定尺寸
+              targetWidth = maxWidth
+              targetHeight = Math.round(maxWidth * (originHeight / originWidth))
+            } else {
+              // 按照高度限定尺寸
+              targetHeight = maxHeight
+              targetWidth = Math.round(maxHeight * (originWidth / originHeight))
+            }
+          }
+          // 3、canvas对图片进行缩放
+          // 缩放图片需要的canvas
+          var canvas = document.createElement('canvas')
+          var context = canvas.getContext('2d')
+          canvas.width = targetWidth
+          canvas.height = targetHeight
+          // 清除画布
+          context.clearRect(0, 0, targetWidth, targetHeight)
+          // 图片压缩
+          context.drawImage(img, 0, 0, targetWidth, targetHeight)
+          /* 第一个参数是创建的img对象；第二个参数是左上角坐标，后面两个是画布区域宽高 */
+          // 压缩后的图片base64
+          /* canvas.toDataURL(mimeType, qualityArgument)
+           *  mimeType 默认值是'image/jpeg';
+           *  qualityArgument表示导出的图片质量，只要导出为jpg和webp格式的时候此参数才有效果，默认值是0.92
+           */
+          // 4、base64 格式(处理方式一)
+          this_.newUrlBase64 = canvas.toDataURL('image/jpeg', 0.92)
+          this_.fileName = file.name
+          this_.temporaryURL = URL.createObjectURL(this_.dataURLtoFile(this_.newUrlBase64, file.name))
+          this_.isShowUploadBtn = false
+
+          // (处理方式二)
+          // 回调函数中可以得到转化后的blob文件，type为要转成的图片类型，默认png。
+          // encoderOptions为当设置的图片格式为 image/jpeg 或者 image/webp 时用来指定图片展示质量。
+          // canvas.toBlob((val) => {
+          //   const url = URL.createObjectURL(val)
+          //   this_.temporaryURL = url
+          //   this_.isShowUploadBtn = false
+          // }, 'image/jpeg', 0.92)
+        }
+      } else {
+        this.$message({
+          type: 'warning',
+          message: '请上传图片'
+        })
+      }
+    },
+
+    // 将base64转换为文件
+    dataURLtoFile (dataurl, filename) {
+      const arr = dataurl.split(',')
+      const mime = arr[0].match(/:(.*?);/)[1]
+      const bstr = atob(arr[1])
+      let n = bstr.length
+      var u8arr = new Uint8Array(n)
+      while (n--) {
+        u8arr[n] = bstr.charCodeAt(n)
+      }
+      return new File([u8arr], filename, { type: mime })
     },
 
     // 新增英雄
     onSubmit () {
-      const formData = new FormData(this.$refs.form.$el)
+      const formData = new FormData()
+      formData.append('images', this.dataURLtoFile(this.newUrlBase64, this.fileName))
+      formData.append('intro', this.form.intro)
+      formData.append('nickName', this.form.nickName)
+      formData.append('userName', this.form.userName)
       this.$store.dispatch('postAddHero', formData).then(response => {
         if (response.data.code === 200) {
           this.$message({
@@ -186,6 +272,10 @@ export default {
           this.initData()
         }
       })
+    },
+
+    onClick (e) {
+      console.log('====', e)
     }
   }
 }
