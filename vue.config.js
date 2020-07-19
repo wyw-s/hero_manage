2ec62ca9c49dbecc434fe042c58cdd8377d3f795
wyw-s <wyw.s@foxmail.com>
2020-07-19 12:39:37 +0800

    关闭生产环境下的sourceMap

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,21 @@
 module.exports = {
   // 解决部署后访问空白页的问题；
   publicPath: '/',
+  // 输出文件目录 默认为: dist
+  outputDir: 'dist',
+  // 放置打包生成的静态资源 (js、css、img、fonts) 的目录。该目录相对于 outputDir 。
+  assetsDir: 'assets',
+  // 为生产环境禁用 SourceMap
+  productionSourceMap: false,
+  // 将打包生成的的静态资源的文件名中加入hash以便控制浏览器缓存问题。
+  filenameHashing: true,
+  // css相关配置
+  css: {
+    // 是否使用css分离插件 ExtractTextPlugin
+    extract: true,
+    // 开启 CSS source maps?
+    sourceMap: false
+  },
   devServer: {
     host: '0.0.0.0', // 允许以 ip 地址访问本机
     port: 8085,
