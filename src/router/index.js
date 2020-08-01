cba6db2a00421814a4bd5b90551a5ebcb2211159
wyw-s <wyw.s@foxmail.com>
2020-08-01 17:26:51 +0800

    新增login路由

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,11 @@ const routes = [
         component: () => import('@/views/heroManage')
       }
     ]
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('@/views/login')
   }
 ]
 
